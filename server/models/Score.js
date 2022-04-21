40f7c4de59981cab4957b392acfcd41b58649246
HWMooo <58176899+HWMooo@users.noreply.github.com>
2022-04-21 16:09:53 +0100

    locating error

--- server/models/Score.js
+++ server/models/Score.js
@@ -13,7 +13,6 @@ class Score {
       try {
         const db = await init();
         const scoresData = await db.collection("scores").find().toArray();
-        node.warn(scoresData)
         let scores = scoresData.map((score) => new Score(score));
         res(scores);
       } catch (error) {
