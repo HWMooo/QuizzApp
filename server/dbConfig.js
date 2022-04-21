1db4a8a8838b78f7f186137931160a88cba20c4f
HWMooo <58176899+HWMooo@users.noreply.github.com>
2022-04-21 16:49:14 +0100

    finding error

--- server/dbConfig.js
+++ server/dbConfig.js
@@ -4,6 +4,7 @@ const connectionUrl = process.env.DB_CONNECTION;
 const dbName = process.env.DB_NAME;
 
 const init = async () => {
+  console.log('error here')
   let client = await MongoClient.connect(connectionUrl);
   console.log("connected to database!", dbName);
   return client.db(dbName);
