25bf9680cf11f4024279eece772d5379f11bb526
HWMooo <58176899+HWMooo@users.noreply.github.com>
2022-04-22 10:54:22 +0100

    Update index.js

--- client/src/pages/Results/index.js
+++ client/src/pages/Results/index.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { emptyQuiz } from "../../actions";
-import './styles.css'
+import './style.css'
 
 function Results() {
   const dispatch = useDispatch();
